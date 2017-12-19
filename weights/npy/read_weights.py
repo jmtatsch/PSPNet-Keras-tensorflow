b6d0c42efb2fa8a712feba86d9bd8d6710ae27d0
Julian Tatsch <julian@tatsch.it>
2017-12-19 13:48:06 +0100

    fixed tab indentations

--- weights/npy/read_weights.py
+++ weights/npy/read_weights.py
@@ -4,12 +4,11 @@ import numpy as np
 weights = np.load("pspnet101_cityscapes.npy").item()
 settable_weights = 0
 for layer, value in weights.items():
-	print(layer)
-	for attrib, vals in weights[layer].items():
-		if attrib == "weights":
-			print("weights: ", vals.shape)
-		else:
-			print(attrib)
-	settable_weights +=1
+    print(layer)
+    for attrib, vals in weights[layer].items():
+        if attrib == "weights":
+            print("weights: ", vals.shape)
+        else:
+            print(attrib)
+    settable_weights += 1
 print("Total settable weights %i" % settable_weights)
-
